b374acee49149db498989292ef67b79868de442f
Marcus <Marcus.poulsen.mpu@gmail.com>
2026-02-21 18:15:06 +0100

    visuelt

--- tabs/daily_summary_tab.py
+++ tabs/daily_summary_tab.py
@@ -6,8 +6,14 @@ def render(df, from_date, to_date, _filter_df_by_view_range):
     daily_summary = df_tab.groupby(df_tab['time'].dt.date).agg({
         'usage_kwh': 'sum',
         'total_udgift': 'sum',
-        'spot_pris': 'mean',
-        'tarif_pris': 'first'
+        'spot_pris': 'mean'
     }).reset_index()
-    daily_summary.columns = ['Date', 'Usage (kWh)', 'Total Cost (DKK)', 'Avg Spot Pris', 'Tarif Pris']
+    daily_summary['gennemsnits strømpris alt inklusiv'] = daily_summary['total_udgift'] / daily_summary['usage_kwh']
+    daily_summary.columns = [
+        'dato',
+        'forbrug (kwh)',
+        'total udgift kr',
+        'gennemsnits spotpris betalt',
+        'gennemsnits strømpris alt inklusiv'
+    ]
     st.dataframe(daily_summary, width='stretch')
