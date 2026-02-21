a9061648616d5fb7c8419ae9daece47af5485617
Marcus <Marcus.poulsen.mpu@gmail.com>
2026-02-21 18:18:30 +0100

    visuelt

--- tabs/hourly_stats_tab.py
+++ tabs/hourly_stats_tab.py
@@ -2,8 +2,7 @@ import streamlit as st
 import plotly.graph_objects as go
 
 def render(df, from_date, to_date, _filter_df_by_view_range):
-    view_range = st.date_input('Vis periode (filter)', value=(from_date, to_date), key='filter_tab5')
-    df_tab = _filter_df_by_view_range(df, view_range)
+    df_tab = df.copy()
     hourly_stats = df_tab.copy()
     hourly_stats['hour_of_day'] = hourly_stats['time'].dt.hour
     avg_by_hour = hourly_stats.groupby('hour_of_day').agg({
@@ -13,7 +12,15 @@ def render(df, from_date, to_date, _filter_df_by_view_range):
         'total_pris_per_kwh': 'mean',
         'total_udgift': 'mean'
     }).reset_index()
-    avg_by_hour.columns = ['Hour', 'Avg Usage (kWh)', 'Avg Spot Pris', 'Tarif Pris', 'Avg Total Pris (DKK/kWh)', 'Avg Total Cost (DKK)']
+    avg_by_hour['gennemsnits strømpris alt inklusiv'] = avg_by_hour['Avg Total Cost (DKK)'] / avg_by_hour['Avg Usage (kWh)']
+    avg_by_hour.columns = [
+        'time',
+        'forbrug (kwh)',
+        'gennemsnits spotpris betalt',
+        'tarif pris',
+        'gennemsnits strømpris alt inklusiv',
+        'total udgift kr'
+    ]
     st.dataframe(avg_by_hour, width='stretch')
     fig = go.Figure()
     fig.add_trace(go.Bar(x=avg_by_hour['Hour'], y=avg_by_hour['Avg Usage (kWh)'], name='Avg Usage (kWh)'))
