07e63bc631444fad2b3fa806166829b384f43fe6
Marcus <Marcus.poulsen.mpu@gmail.com>
2026-02-21 18:16:26 +0100

    visuelt

--- tabs/daily_summary_tab.py
+++ tabs/daily_summary_tab.py
@@ -1,8 +1,7 @@
 import streamlit as st
 
 def render(df, from_date, to_date, _filter_df_by_view_range):
-    view_range = st.date_input('Filtrer på en bestemt periode', value=(from_date, to_date), key='filter_tab4')
-    df_tab = _filter_df_by_view_range(df, view_range)
+    df_tab = df.copy()
     daily_summary = df_tab.groupby(df_tab['time'].dt.date).agg({
         'usage_kwh': 'sum',
         'total_udgift': 'sum',
